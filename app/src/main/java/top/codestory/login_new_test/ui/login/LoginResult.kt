af225f86b6fcdefd8a857f1d333e43520bf23e34
aoian5173 <2926295173@qq.com>
2022-04-17 14:37:02 +0800

    Initial commit

---
+++ app/src/main/java/top/codestory/login_new_test/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package top.codestory.login_new_test.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
